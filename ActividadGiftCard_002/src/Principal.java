35cad713d3d4af60111c04a98bbb2f6da9194411
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-09-07 12:13:39 -0300

    Update Principal.java

--- ActividadGiftCard_002/src/Principal.java
+++ ActividadGiftCard_002/src/Principal.java
@@ -2,16 +2,24 @@
 public class Principal {
     public static void main(String[] args) {
         long codigoAuxiliar = 1000000000000000l;
-        Trabajador t1 = new Trabajador(1000, "K", "Juan");
-        Trabajador t2 = new Trabajador(1200, "1", "Ana");
+        Trabajador t1 = new Trabajador(10500600, "K", "Juan");
+        Trabajador t2 = new Trabajador(1200800, "1", "Ana");
         
         System.out.println(codigoAuxiliar);
         GiftCard g1 = new GiftCard();
         codigoAuxiliar = g1.generarCodigo(codigoAuxiliar);
+        g1.setTrabajador(t1);
+        g1.generarClave();
         System.out.println(codigoAuxiliar);
         GiftCard g2 = new GiftCard();
         codigoAuxiliar = g2.generarCodigo(codigoAuxiliar);
+        g2.setTrabajador(t2);
+        g2.generarClave();
         System.out.println(codigoAuxiliar);
+        
+        System.out.println(g1.toString());
+        System.out.println(g2);
+        
         
     }
 }
