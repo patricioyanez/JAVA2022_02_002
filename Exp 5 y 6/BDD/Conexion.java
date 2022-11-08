170fb3388144ac4340ae621e0e11154fb9afed77
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-11-08 16:56:25 -0300

    UPDATE

---
+++ Exp 5 y 6/BDD/Conexion.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author administrador
+ */
+public class Conexion {
+
+    public Connection obtenerConexion() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/PGY2121", "root", "");
+            System.out.println("Conexión exitosa");
+        } catch (SQLException e) {
+            System.out.println("Error de conexión" + e.getMessage());
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return connection;
+    }
+}
